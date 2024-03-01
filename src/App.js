7f6475a32a0d4349f953f96bcb38fca9f64b0e54
Kastoma <project.sat.era@proton.me>
2024-03-01 03:24:52 -0800

    Additional files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import store from './admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import MainAdminPage from './page/MainAdminPage';
+
+class App extends React.Component{  
+  render(){return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainAdminPage />} />
+       </Routes>
+    </Router>
   );
-}
+}}
 
 export default App;
+    
